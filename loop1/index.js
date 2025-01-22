14eed1dd71e2ebd1003f50b05db0cfef7ad6a8d0
sameer ali <sameeraly240@gmail.com>
2025-01-22 17:17:44 +0500

    add

---
+++ loop1/index.js
@@ -0,0 +1,16 @@
+let total=0
+let num=10
+for(i=1;i<=num;i++){
+    total=total+i
+}
+console.log(total);
+
+
+for(let i=0; i<=10; i++){
+    if(i===4){
+        continue;
+    }
+    console.log(i);
+    
+}
+console.log("hello");
