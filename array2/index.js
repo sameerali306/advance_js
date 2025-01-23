2d198b5e630fa1688e342f60ea2faf6da4623d6d
sameer ali <sameeraly240@gmail.com>
2025-01-23 10:56:23 +0500

    sameer

---
+++ array2/index.js
@@ -0,0 +1,13 @@
+let fruits=["apple","mango","grap","pine apple"]
+let num =[1,2,3,45]
+console.log(fruits[2]);
+console.log(num[2]);
+
+let fname=["sameer","naeem","sohail","danish"]
+console.log(fname);
+fname[1]="amir"
+console.log(typeof fname);
+console.log(Array.isArray(fname));
+let remove=fname.unshift("zohaib")
+console.log(fname);
+console.log("remoe fruit is",remove);
